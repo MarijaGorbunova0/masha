46f1adf16ca9a04f6a48c469588ed6acc8ed7693
MarijaGorbunova0 <153904965+MarijaGorbunova0@users.noreply.github.com>
2024-01-06 18:45:35 +0200

    Add files via upload

---
+++ work1.py
@@ -0,0 +1,68 @@
+#1
+#print("Tere maailm")
+#nimi=input("Mis on sinu nimi?") #comment
+#vanus=int(input("Kui vana sa oled?")) #float()->2.5
+#print("Tere tulemas! "+nimi+". Sa oled"+str(vanus)+" aastat vana")
+#print("Tere tulemast! ",nimi,". Sa oled",vanus,"aasatat vana")
+#print("Tere tulemast! {0}.Sa oled {1} aastat vana".format(nimi,vanus))
+#print("Muutuja vanus",vanus,"tüüp on",type(vanus))
+
+#2
+#vanus= 18
+#eesnimi = "Jaak"
+#pikkus = 16.5
+#Kas käib kollis = True
+#print("Muutuja vanus",eesnimi,"tüüp on",type(eesnimi))
+#print("Muutuja vanus",pikkus,"tüüp on",type(pikkus))
+#print("Muutuja vanus",kas_käib_koolis,"tüüp on",type(kas_käib_koolis))
+#print("Muutuja vanus",pikkus,"tüüp on",type(pikkus))
+
+#3
+#from random import *
+#konfeti=randint(10,100)
+#print("konfeti",konfeti)
+#vzal=int(input("skolko vzal"))
+#konfeti-=vzal
+#print("siin on","konfet")
+#print()
+#user = int(input("длинна окружности дерева - "))
+#kokku = user/3.14
+#print("диаметр окружности равен:", kokku)
+
+#5
+#N = int(input("первое число"))
+#M = int(input("первое второе число"))
+#kokku = N*M
+#print("диагонали прямоугольного участка земли равен", kokku)
+
+#6
+#aeg = float(input("Сколько часов ушло на поездку?"))
+#tee = float(input("Сколько километров ты проехал?"))
+#kiirus= aeg/tee
+#print(kiirus)
+
+#7
+#numbers = (1,2,3,4,5)
+#kokku = sum(numbers)/len(numbers)
+#print(kokku)
+
+#8
+#frog ="""
+#@..@
+#(----)
+#( __/ )
+#^^ "" ^^
+#"""
+
+#9
+#a = int(input("первое число"))
+#b = int(input("второе число"))
+#c = int(input("третье число"))
+#kokku = a+b+c
+#print("периметр треугольника:", kokku)
+
+#10
+#Pitsa = 12.90
+#kokku = Pitsa/10
+#money = (Pitsa+kokku)/2
+#print(money)
